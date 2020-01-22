bf901e388d75efa4e59ea471ee23a25aa81aa2c3
TakeshimaNahoko <takeshima.nahoko@lancers.co.jp>
2020-01-23 00:42:00 +0900

    feat: フォーム配列コントロールへのアクセス

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -20,9 +21,22 @@ export class ProfileEditorComponent implements OnInit {
       state: [''],
       zip: ['']
     }),
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
   });
 
   ngOnInit() {
+  }
+
+  get aliases() {
+    // 返されたコントロールはAbstractControl型
+    // フォーム配列インスタンスのメソッド構文にアクセスするには、明確な型を指定する必要あり
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
   }
 
   updateProfile() {
