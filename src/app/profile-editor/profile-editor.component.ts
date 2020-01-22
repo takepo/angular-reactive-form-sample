e0c583fa10995a3412e5a05346256efddfa1058c
TakeshimaNahoko <takeshima.nahoko@lancers.co.jp>
2020-01-23 00:37:25 +0900

    feat: 必須フィールドを作成する

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -11,7 +12,7 @@ export class ProfileEditorComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   profileForm = this.fb.group({
-    firstName: [''],
+    firstName: ['', Validators.required],
     lastName: [''],
     address: this.fb.group({
       street: [''],
